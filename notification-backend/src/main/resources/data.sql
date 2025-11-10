79a0e0ef176dd4a5d4a862daeb6ca0e68fc09ee2
Malte <malte.braig@student.uibk.ac.at>
2025-11-10 20:17:09 +0100

    adding more resorts

--- notification-backend/src/main/resources/data.sql
+++ notification-backend/src/main/resources/data.sql
@@ -1,75 +1,141 @@
--- FREIZEITTICKET TIROL & SNOWCARD TIROL SKI RESORTS
+-- COMPLETE LIST: FREIZEITTICKET TIROL & SNOWCARD TIROL SKI RESORTS
+-- All resorts included in either or both passes (duplicates removed)
+-- Coordinates are base station/main lift locations where available
+-- Elevation is maximum altitude (top station)
+
 INSERT INTO ski_resort (name, latitude, longitude, elevation) VALUES 
 
--- Major Glaciers (in both passes)
+-- ========== GLACIERS (in both passes) ==========
 ('Stubai Glacier', 47.0089, 11.1181, 3210),
-('Pitztal Glacier', 46.9239, 10.8786, 3440),
 ('Hintertux Glacier', 47.0744, 11.6581, 3250),
-('Sölden', 46.9691, 11.0039, 3058),
+('Pitztal Glacier', 46.9239, 10.8786, 3440),
 ('Kaunertal Glacier', 46.9347, 10.7167, 3113),
+('Sölden', 46.9691, 11.0039, 3058),
 
--- Freizeitticket Tirol Resorts (around Innsbruck)
+-- ========== INNSBRUCK REGION (Freizeitticket + Snowcard) ==========
 ('Nordkette', 47.3050, 11.3839, 2256),
-('Axamer Lizum', 47.1936, 11.2968, 2340),
-('Kühtai', 47.2089, 11.0142, 2520),
 ('Patscherkofel', 47.2089, 11.4608, 2246),
+('Axamer Lizum', 47.1936, 11.2968, 2340),
 ('Glungezer', 47.2878, 11.5669, 2677),
 ('Schlick 2000', 47.1622, 11.3106, 2240),
 ('Serles', 47.1519, 11.3419, 2717),
-('Rangger Köpfl', 47.2478, 11.1483, 2000),
 ('Muttereralm', 47.2322, 11.3808, 1600),
+('Rangger Köpfl', 47.2478, 11.1483, 2000),
+('Bergeralm', 47.2892, 11.4639, 2000),
 
--- Ötztal Valley (Freizeitticket & Snowcard)
+-- ========== KÜHTAI & SELLRAINTAL (Freizeitticket + Snowcard) ==========
+('Kühtai', 47.2089, 11.0142, 2520),
+
+-- ========== ÖTZTAL VALLEY (both passes) ==========
 ('Hochoetz', 47.2022, 10.8744, 2272),
 ('Obergurgl-Hochgurgl', 46.8681, 11.0264, 3082),
 ('Vent', 46.8569, 10.9092, 1900),
+('Niederthai', 47.1650, 10.8970, 2178),
 
--- Paznaun Valley (Freizeitticket & Snowcard)
+-- ========== PAZNAUN VALLEY (Freizeitticket + Snowcard) ==========
 ('Ischgl-Samnaun', 46.9914, 10.2992, 2872),
 ('Galtür', 46.9681, 10.1853, 2297),
 ('Kappl', 47.0631, 10.3758, 2690),
 ('See', 47.0786, 10.4347, 2456),
 
--- Ski Arlberg (Freizeitticket - limited access)
+-- ========== SKI ARLBERG (Freizeitticket - limited, Snowcard - full) ==========
 ('St. Anton am Arlberg', 47.1275, 10.2642, 2811),
+('Lech-Zürs', 47.1597, 10.1397, 2450),
+('St. Christoph', 47.1219, 10.2936, 2811),
+('Stuben', 47.1200, 10.3050, 2450),
+('Warth-Schröcken', 47.2569, 10.1878, 2085),
 
--- Major Snowcard Tirol Resorts
-('Kitzbühel-Kirchberg', 47.4386, 12.3928, 2000),
-('SkiWelt Wilder Kaiser', 47.5078, 12.1892, 1957),
-('Zillertal Arena', 47.2256, 11.9417, 2500),
-('Mayrhofen', 47.1656, 11.8614, 2500),
-('Ski Juwel Alpbachtal', 47.3953, 11.9344, 2025),
-('Spieljoch', 47.1894, 11.7544, 2236),
-('Hochfügen-Hochzillertal', 47.1531, 11.7936, 2500),
-('Gerlos', 47.2306, 12.0358, 2166),
-
--- Serfaus-Fiss-Ladis (Snowcard)
+-- ========== SERFAUS-FISS-LADIS (Snowcard) ==========
 ('Serfaus-Fiss-Ladis', 47.0375, 10.6061, 2828),
 
--- Nauders (Snowcard)
+-- ========== NAUDERS & KAUNERTAL (Snowcard) ==========
 ('Nauders', 46.8892, 10.5042, 2850),
+('Fendels', 47.0844, 10.5281, 2613),
+('Venet', 47.1319, 10.5611, 2512),
 
--- Kaunertal (Snowcard)
-('Kaunertal Glacier', 46.9347, 10.7167, 3113),
-
--- East Tyrol Resorts (Snowcard)
-('Großglockner Resort Kals-Matrei', 47.0114, 12.6708, 2621),
-('Lienz Hochstein', 46.8292, 12.7756, 2050),
-('St. Jakob in Defereggen', 46.9231, 12.3542, 2525),
-
--- Kitzbühel Region (Snowcard)
-('St. Johann in Tirol', 47.5228, 12.4208, 1696),
-('Fieberbrunn', 47.4642, 12.5589, 2020),
-
--- Additional Notable Resorts (Snowcard)
-('Hintertuxer Gletscher', 47.0744, 11.6581, 3250),
+-- ========== ZILLERTAL REGION (Snowcard) ==========
+('Zillertal Arena', 47.2256, 11.9417, 2500),
+('Mayrhofen-Hippach', 47.1656, 11.8614, 2500),
+('Hochfügen-Hochzillertal', 47.1531, 11.7936, 2500),
+('Spieljoch-Fügen', 47.1894, 11.7544, 2236),
 ('Kaltenbach-Hochzillertal', 47.2792, 11.8786, 2500),
-('Achensee', 47.4419, 11.7083, 1500),
+('Gerlos', 47.2306, 12.0358, 2166),
+('Zell-Gerlos', 47.2306, 12.0358, 2166),
+('Ski Optimal Hochfügen', 47.1531, 11.7936, 2500),
+('Zillertal 3000', 47.0744, 11.6581, 3250),
+
+-- ========== ALPBACHTAL & WILDSCHÖNAU (Snowcard) ==========
+('Ski Juwel Alpbachtal-Wildschönau', 47.3953, 11.9344, 2025),
 ('Alpbach', 47.3953, 11.9444, 2025),
-('Pertisau', 47.4139, 11.7175, 1900),
-('Kramsach', 47.4550, 11.8792, 1456),
+('Wildschönau', 47.4319, 12.0392, 1850),
 ('Reith im Alpbachtal', 47.3836, 11.9142, 1456),
 
--- Smaller Resorts around Innsbruck (Freizeitticket)
-('Bergeralm', 47.2892, 11.4639, 2000),
-('Hochstein Lienz', 46.8292, 12.7756, 2050);
+-- ========== KITZBÜHEL REGION (Snowcard) ==========
+('KitzSki Kitzbühel-Kirchberg', 47.4464, 12.3922, 2000),
+('St. Johann in Tirol', 47.5228, 12.4208, 1696),
+('Fieberbrunn', 47.4642, 12.5589, 2020),
+('Kirchberg in Tirol', 47.4503, 12.3167, 2000),
+('Pass Thurn', 47.2367, 12.4692, 2174),
+
+-- ========== SKIWELT WILDER KAISER-BRIXENTAL (Snowcard) ==========
+('SkiWelt Wilder Kaiser-Brixental', 47.5078, 12.1892, 1957),
+('Söll', 47.5050, 12.1950, 1829),
+('Ellmau', 47.5153, 12.2972, 1555),
+('Going', 47.5089, 12.3228, 1731),
+('Scheffau', 47.4806, 12.2428, 1750),
+('Westendorf', 47.4319, 12.2136, 1892),
+('Brixen im Thale', 47.4492, 12.2556, 1856),
+('Hopfgarten', 47.4492, 12.1592, 1829),
+('Itter', 47.4658, 12.1347, 1200),
+('Hohe Salve', 47.4378, 12.1267, 1828),
+
+-- ========== ACHENSEE REGION (Snowcard) ==========
+('Achensee - Christlum', 47.4419, 11.7083, 1800),
+('Achensee - Pertisau', 47.4139, 11.7175, 1900),
+('Rofan', 47.4681, 11.7756, 2259),
+
+-- ========== WIPPTAL (Freizeitticket + Snowcard) ==========
+('Obernberg am Brenner', 47.0181, 11.4308, 2015),
+('Steinach am Brenner', 47.0878, 11.4714, 2100),
+
+-- ========== EAST TYROL/OSTTIROL (Snowcard) ==========
+('Großglockner Resort Kals-Matrei', 47.0114, 12.6708, 2621),
+('Lienz - Hochstein', 46.8292, 12.7756, 2050),
+('Lienz - Zettersfeld', 46.8350, 12.7875, 2278),
+('St. Jakob in Defereggen', 46.9231, 12.3542, 2525),
+('Sillian-Hochpustertal', 46.7539, 12.4233, 2407),
+('Obertilliach', 46.7069, 12.6133, 2048),
+('Kartitsch', 46.7075, 12.4997, 2100),
+
+-- ========== TIROLER OBERLAND (Snowcard) ==========
+('Imst-Hoch-Imst', 47.2267, 10.7356, 2050),
+('Hahnenkamm Reutte', 47.4767, 10.7222, 1938),
+
+-- ========== TIROLER ZUGSPITZ ARENA (Snowcard) ==========
+('Ehrwald - Zugspitze', 47.4006, 10.9308, 2962),
+('Lermoos', 47.4092, 10.8819, 2100),
+('Berwang', 47.4036, 10.7667, 1860),
+('Bichlbach', 47.4219, 10.7711, 1600),
+('Biberwier', 47.3711, 10.8928, 2100),
+('Heiterwang', 47.4564, 10.7500, 1600),
+
+-- ========== SEEFELD REGION (Snowcard) ==========
+('Rosshütte Seefeld', 47.3228, 11.1919, 2064),
+('Gschwandtkopf', 47.3386, 11.1706, 1500),
+
+-- ========== PITZTAL (both passes) ==========
+('Rifflsee', 46.9900, 10.8553, 2880),
+('Hochzeiger', 47.0578, 10.8344, 2560),
+
+-- ========== KLEINERE SKIGEBIETE (Various smaller areas) ==========
+('Hochkössen', 47.6528, 12.4200, 1670),
+('Zahmer Kaiser', 47.6644, 12.2944, 1425),
+('Thiersee', 47.6164, 12.1128, 1280),
+('Kramsach', 47.4550, 11.8792, 1456),
+('Weerberg', 47.2594, 11.6506, 1850),
+('Kolsassberg', 47.2881, 11.6183, 1456),
+('Lizum-Waldrast', 47.1267, 11.4556, 2400),
+('Rosskogelhütte Lüsens', 47.1525, 11.2031, 2350),
+('Kaiserbergbahn Walchsee', 47.6500, 12.3081, 1100),
+('Pillersee', 47.5133, 12.5611, 1663),
+('Steinplatte', 47.5156, 12.5619, 1869);
