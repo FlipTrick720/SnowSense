d786bd27b228557b6004e918f4ac323ceb83ed7c
Malte <malte.braig@student.uibk.ac.at>
2025-11-10 14:12:13 +0100

    initial data insert for H2

---
+++ notification-backend/src/main/resources/data.sql
@@ -0,0 +1,75 @@
+-- FREIZEITTICKET TIROL & SNOWCARD TIROL SKI RESORTS
+INSERT INTO ski_resort (name, latitude, longitude, elevation) VALUES 
+
+-- Major Glaciers (in both passes)
+('Stubai Glacier', 47.0089, 11.1181, 3210),
+('Pitztal Glacier', 46.9239, 10.8786, 3440),
+('Hintertux Glacier', 47.0744, 11.6581, 3250),
+('Sölden', 46.9691, 11.0039, 3058),
+('Kaunertal Glacier', 46.9347, 10.7167, 3113),
+
+-- Freizeitticket Tirol Resorts (around Innsbruck)
+('Nordkette', 47.3050, 11.3839, 2256),
+('Axamer Lizum', 47.1936, 11.2968, 2340),
+('Kühtai', 47.2089, 11.0142, 2520),
+('Patscherkofel', 47.2089, 11.4608, 2246),
+('Glungezer', 47.2878, 11.5669, 2677),
+('Schlick 2000', 47.1622, 11.3106, 2240),
+('Serles', 47.1519, 11.3419, 2717),
+('Rangger Köpfl', 47.2478, 11.1483, 2000),
+('Muttereralm', 47.2322, 11.3808, 1600),
+
+-- Ötztal Valley (Freizeitticket & Snowcard)
+('Hochoetz', 47.2022, 10.8744, 2272),
+('Obergurgl-Hochgurgl', 46.8681, 11.0264, 3082),
+('Vent', 46.8569, 10.9092, 1900),
+
+-- Paznaun Valley (Freizeitticket & Snowcard)
+('Ischgl-Samnaun', 46.9914, 10.2992, 2872),
+('Galtür', 46.9681, 10.1853, 2297),
+('Kappl', 47.0631, 10.3758, 2690),
+('See', 47.0786, 10.4347, 2456),
+
+-- Ski Arlberg (Freizeitticket - limited access)
+('St. Anton am Arlberg', 47.1275, 10.2642, 2811),
+
+-- Major Snowcard Tirol Resorts
+('Kitzbühel-Kirchberg', 47.4386, 12.3928, 2000),
+('SkiWelt Wilder Kaiser', 47.5078, 12.1892, 1957),
+('Zillertal Arena', 47.2256, 11.9417, 2500),
+('Mayrhofen', 47.1656, 11.8614, 2500),
+('Ski Juwel Alpbachtal', 47.3953, 11.9344, 2025),
+('Spieljoch', 47.1894, 11.7544, 2236),
+('Hochfügen-Hochzillertal', 47.1531, 11.7936, 2500),
+('Gerlos', 47.2306, 12.0358, 2166),
+
+-- Serfaus-Fiss-Ladis (Snowcard)
+('Serfaus-Fiss-Ladis', 47.0375, 10.6061, 2828),
+
+-- Nauders (Snowcard)
+('Nauders', 46.8892, 10.5042, 2850),
+
+-- Kaunertal (Snowcard)
+('Kaunertal Glacier', 46.9347, 10.7167, 3113),
+
+-- East Tyrol Resorts (Snowcard)
+('Großglockner Resort Kals-Matrei', 47.0114, 12.6708, 2621),
+('Lienz Hochstein', 46.8292, 12.7756, 2050),
+('St. Jakob in Defereggen', 46.9231, 12.3542, 2525),
+
+-- Kitzbühel Region (Snowcard)
+('St. Johann in Tirol', 47.5228, 12.4208, 1696),
+('Fieberbrunn', 47.4642, 12.5589, 2020),
+
+-- Additional Notable Resorts (Snowcard)
+('Hintertuxer Gletscher', 47.0744, 11.6581, 3250),
+('Kaltenbach-Hochzillertal', 47.2792, 11.8786, 2500),
+('Achensee', 47.4419, 11.7083, 1500),
+('Alpbach', 47.3953, 11.9444, 2025),
+('Pertisau', 47.4139, 11.7175, 1900),
+('Kramsach', 47.4550, 11.8792, 1456),
+('Reith im Alpbachtal', 47.3836, 11.9142, 1456),
+
+-- Smaller Resorts around Innsbruck (Freizeitticket)
+('Bergeralm', 47.2892, 11.4639, 2000),
+('Hochstein Lienz', 46.8292, 12.7756, 2050);
